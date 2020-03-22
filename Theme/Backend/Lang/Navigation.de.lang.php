1117d2cdfbd11311d01c87c899be7ab97e1d6837
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-03-22 18:42:34 +0100

    implement localization

---
+++ Theme/Backend/Lang/Navigation.de.lang.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.4
+ *
+ * @package   Modules\Home
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
+declare(strict_types=1);
+
+return ['Navigation' => [
+    'Home'   => 'Home',
+    'Logout' => 'Logout',
+]];
