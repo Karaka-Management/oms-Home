ed51f522445bc946b8ba321eaab5b69736305720
Dennis Eichhorn <spl1nes.com@googlemail.com>
2021-09-19 19:50:14 +0200

    september update 1

--- Admin/Routes/Web/Backend.php
+++ Admin/Routes/Web/Backend.php
@@ -1,4 +1,55 @@
-<?php declare(strict_types=1);
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 8.0
+ *
+ * @package   Modules
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
+declare(strict_types=1);
+
+use Modules\Home\Controller\BackendController;
+use phpOMS\Account\PermissionType;
+use phpOMS\Router\RouteVerb;
 
 return [
+	'^.*/legal/privacy.*$' => [
+        [
+            'dest'       => '\Modules\Home\Controller\BackendController:viewLegalDocuments',
+            'verb'       => RouteVerb::GET,
+            'permission' => [
+                'module' => BackendController::MODULE_NAME,
+                'type'   => PermissionType::READ,
+                'state'  => null,
+            ],
+        ],
+    ],
+
+    '^.*/legal/terms.*$' => [
+        [
+            'dest'       => '\Modules\Home\Controller\BackendController:viewLegalDocuments',
+            'verb'       => RouteVerb::GET,
+            'permission' => [
+                'module' => BackendController::MODULE_NAME,
+                'type'   => PermissionType::READ,
+                'state'  => null,
+            ],
+        ],
+    ],
+
+    '^.*/legal/imprint.*$' => [
+        [
+            'dest'       => '\Modules\Home\Controller\BackendController:viewLegalDocuments',
+            'verb'       => RouteVerb::GET,
+            'permission' => [
+                'module' => BackendController::MODULE_NAME,
+                'type'   => PermissionType::READ,
+                'state'  => null,
+            ],
+        ],
+    ],
 ];
